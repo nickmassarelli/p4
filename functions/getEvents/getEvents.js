c1ff0a4e9bfde9622348dc429025cc1867184598
nickmassarelli <nickmass93@gmail.com>
2020-11-03 18:20:48 -0500

    n

--- functions/getEvents/getEvents.js
+++ functions/getEvents/getEvents.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch')
-const { format, compareAsc } = require('date-fns')
+// const { format, compareAsc } = require('date-fns')
 
 const endpoint =
   'https://api.sheety.co/9a19cc691e17a75dcd0a6f68b6d7c2d8/p4/sheet1'
@@ -10,37 +10,37 @@ exports.handler = async (event, context) => {
 
     let events = json.events
       .filter((event) => event.approved)
-      .map((event) => {
-        event.startDatetime = new Date(`${event.date}T${event.startTime}`)
-        event.endDatetime = new Date(`${event.date}T${event.endTime}`)
-        event.date = format(event.startDatetime, 'EEEE, LLLL d, y')
-        event.time = `${format(event.startDatetime, 'h:mma')} – ${format(
-          event.endDatetime,
-          'h:mma',
-        )}`.toLowerCase()
-        return event
-      })
-      .filter((event) => compareAsc(event.endDatetime, Date.now()) > -1)
-      .sort((a, b) => compareAsc(a.startDatetime, b.startDatetime))
-      .map((event) => {
-        const start = format(event.startDatetime, "yyyyMMdd'T'HHmmSS")
-        const end = format(event.endDatetime, "yyyyMMdd'T'HHmmSS")
-        event.calandarLink = googleCalendarUrl({
-          title: event.title,
-          start,
-          end,
-          details: event.location
-            ? 'Zoom Link: ' + event.location + ' — ' + event.description
-            : event.description,
-          ctz: 'America/New_York',
-        })
-        if (event.location) {
-          event.zoomId = getZoomID(event.location)
-        } else {
-          event.zoomId = null
-        }
-        return event
-      })
+      // .map((event) => {
+      //   event.startDatetime = new Date(`${event.date}T${event.startTime}`)
+      //   event.endDatetime = new Date(`${event.date}T${event.endTime}`)
+      //   event.date = format(event.startDatetime, 'EEEE, LLLL d, y')
+      //   event.time = `${format(event.startDatetime, 'h:mma')} – ${format(
+      //     event.endDatetime,
+      //     'h:mma',
+      //   )}`.toLowerCase()
+      //   return event
+      // })
+      // .filter((event) => compareAsc(event.endDatetime, Date.now()) > -1)
+      // .sort((a, b) => compareAsc(a.startDatetime, b.startDatetime))
+      // .map((event) => {
+      //   const start = format(event.startDatetime, "yyyyMMdd'T'HHmmSS")
+      //   const end = format(event.endDatetime, "yyyyMMdd'T'HHmmSS")
+      //   event.calandarLink = googleCalendarUrl({
+      //     title: event.title,
+      //     start,
+      //     end,
+      //     details: event.location
+      //       ? 'Zoom Link: ' + event.location + ' — ' + event.description
+      //       : event.description,
+      //     ctz: 'America/New_York',
+      //   })
+      //   if (event.location) {
+      //     event.zoomId = getZoomID(event.location)
+      //   } else {
+      //     event.zoomId = null
+      //   }
+      //   return event
+      // })
 
     return {
       statusCode: 200,
@@ -64,37 +64,37 @@ const datePattern = /^\d{8}(T\d{6}Z?)?$/
 /**
  * Generate a Google Calendar event URL
  */
-function googleCalendarUrl(args) {
-  const { start, end, title, ...rest } = args
-  if (start && !end) {
-    throw new Error('`end` is required when `start` is provided')
-  }
-  if (!start && end) {
-    throw new Error('`start` is required when `end` is provided')
-  }
-  if (start && !datePattern.test(start)) {
-    throw new Error('`start` is malformed')
-  }
-  if (end && !datePattern.test(end)) {
-    throw new Error('`end` is malformed')
-  }
-  if (start && end && start.length !== end.length) {
-    throw new Error('`start` and `end` should be of the same format')
-  }
-  const searchParams = Object.assign(
-    {
-      action: 'TEMPLATE',
-      dates: start && end ? `${start}/${end}` : undefined,
-      text: title,
-    },
-    rest,
-  )
-  const urlSearchParams = new URLSearchParams(clean(searchParams))
-  return (
-    'https://calendar.google.com/calendar/event?' + urlSearchParams.toString()
-  )
-}
+// function googleCalendarUrl(args) {
+//   const { start, end, title, ...rest } = args
+//   if (start && !end) {
+//     throw new Error('`end` is required when `start` is provided')
+//   }
+//   if (!start && end) {
+//     throw new Error('`start` is required when `end` is provided')
+//   }
+//   if (start && !datePattern.test(start)) {
+//     throw new Error('`start` is malformed')
+//   }
+//   if (end && !datePattern.test(end)) {
+//     throw new Error('`end` is malformed')
+//   }
+//   if (start && end && start.length !== end.length) {
+//     throw new Error('`start` and `end` should be of the same format')
+//   }
+//   const searchParams = Object.assign(
+//     {
+//       action: 'TEMPLATE',
+//       dates: start && end ? `${start}/${end}` : undefined,
+//       text: title,
+//     },
+//     rest,
+//   )
+//   const urlSearchParams = new URLSearchParams(clean(searchParams))
+//   return (
+//     'https://calendar.google.com/calendar/event?' + urlSearchParams.toString()
+//   )
+// }
 
-function getZoomID(url) {
-  return new URL(url).pathname.slice(3)
-}
+// function getZoomID(url) {
+//   return new URL(url).pathname.slice(3)
+// }
